f4873c72447c024740bc92acbbc8b370473ebcb6
kalculata <nprincehuzaifa@gmail.com>
2021-08-27 17:05:05 +0200

    change components folder

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+Vue.component('c-header', {
+    template: `
+    <header>
+        <div class="logo">
+
+        </div>
+        <ul class="nav">
+            <li><a href="orders.html">Orders</a></li>
+            <li><a href="products.html">Products</a></li>
+            <li><a href="users.html">Users</a></li>
+        </ul>
+        <div class="actions">
+            <a href="messages.html"><i class="far fa-envelope"></i></a>
+            <span class="message_counter">9+</span>
+        </div>
+    </header>
+    `
+});
